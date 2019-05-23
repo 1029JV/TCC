d6560c7cd36d814db93dc10ac56293369dceaac8
1029JV <joao_vitor1029@outlook.com>
2019-05-22 21:41:25 -0300

    Atualizacao2

--- mapa.php
+++ mapa.php
@@ -17,96 +17,11 @@
         include("header.html");
         ?>
 
-        <section class="mt-3 py-3">
-            <h2 class="text-center">Mapa</h2>
-            <div class="mr-5 float-right text-center">
-                <p>Temperatura em <strong>°C</strong></p>
-                <table class="table table-sm" style="color: black; font-weight: bolder;">
-                    <tbody>
-                        <tr>
-                            <td style="background-color: rgb(182, 0, 0);">
-                                <p>39 +</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 55, 0);">
-                                <p>39 a 36</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 110, 0);">
-                                <p>36 a 33</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 165, 0);">
-                                <p>33 a 30</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 195, 0);">
-                                <p>30 a 27</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 225, 0);">
-                                <p>27 a 24</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(255, 255, 0);">
-                                <p>24 a 21</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(170, 213, 0);">
-                                <p>21 a 18</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(85, 170, 0);">
-                                <p>18 a 15</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(0, 128, 0);">
-                                <p>15 a 12</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(0, 85, 85);">
-                                <p>12 a 9</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(0, 43, 170);">
-                                <p>9 a 6</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(0, 0, 255);">
-                                <p>6 a 3</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(45, 69, 248);">
-                                <p>3 a 0</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(90, 137, 242);">
-                                <p>0 a -3</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="background-color: rgb(135, 206, 235);">
-                                <p>-3</p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <section class="mt-3 py-3 container">
+            <div class=" text-center">
+                <h2>Mapa</h2>
+                <iframe src="http://www.inmet.gov.br/sonabra/maps/pg_mapa.php" width="100%" height="600px" frameborder="0"></iframe>
             </div>
-            <img src="https://s3.amazonaws.com/img.tempoagora.com.br/temperatura.png?1557174823026" style="z-index: 2; opacity: 0.5; transform: translate3d(110px, 0px, 0px); width: 1024px; height: 830px;">
         </section>
 
         <?php
