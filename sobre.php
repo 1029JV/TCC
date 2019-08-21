6d51dff4b6c355d04587407a7d0f0989756128f2
1029JV <joao_vitor1029@outlook.com>
2019-08-21 16:07:24 -0300

    Atualizacao
    Atualizacao de imagens e conteúdo

--- sobre.php
+++ sobre.php
@@ -6,8 +6,6 @@
         <meta name="msapplication-navbutton-color" content="#085231">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
         <link rel="icon" href="img/icon.png">
         <title>TMI - Sobre</title>
     </head>
